b12686d8dc1848625f86b3123903f0b101309deb
alejandro Martinez <alejandro2ml22@gmail.com>
2021-05-06 17:52:05 -0600

    agregando configuración en laravel para conectarse con Oracle DB

---
+++ sistemaPlanilla/config/oracle.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'oracle' => [
+        'driver'            => 'oracle',
+        'host'              => env('DB_HOST', ''),
+        'port'              => env('DB_PORT', '1521'),
+        'port'              => '1521',
+        'database'          => env('DB_DATABASE', ''),
+        'service_name'	=> 'ORCLPDB1.localdomain',
+        'username'          => env('DB_USERNAME', ''),
+        'password'          => env('DB_PASSWORD', ''),
+        'charset'           => env('DB_CHARSET', 'AL32UTF8'),
+        'prefix'            => env('DB_PREFIX', ''),
+    ],
+];
