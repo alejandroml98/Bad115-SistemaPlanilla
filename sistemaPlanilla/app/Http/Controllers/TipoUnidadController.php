395111d77af37363f4bee7bd2db50525e8216c3e
PDePaz <pvelasquez@applaudostudios.com>
2021-05-23 18:21:22 -0600

    :hammer: Se agrego controllers para Puesto, RangoSalarial, Rol, RolPrivilegio, TipoUnidad, Unidad_CentroCostos, Unidad

---
+++ sistemaPlanilla/app/Http/Controllers/TipoUnidadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TipoUnidad;
+use Illuminate\Http\Request;
+
+class TipoUnidadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TipoUnidad  $tipoUnidad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoUnidad $tipoUnidad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TipoUnidad  $tipoUnidad
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TipoUnidad $tipoUnidad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TipoUnidad  $tipoUnidad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoUnidad $tipoUnidad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TipoUnidad  $tipoUnidad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoUnidad $tipoUnidad)
+    {
+        //
+    }
+}
---
+++ sistemaPlanilla/app/Http/Controllers/UnidadCentroCostosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Unidad_CentroCostos;
+use Illuminate\Http\Request;
+
+class UnidadCentroCostosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Unidad_CentroCostos  $unidad_CentroCostos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Unidad_CentroCostos $unidad_CentroCostos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Unidad_CentroCostos  $unidad_CentroCostos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Unidad_CentroCostos $unidad_CentroCostos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Unidad_CentroCostos  $unidad_CentroCostos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Unidad_CentroCostos $unidad_CentroCostos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Unidad_CentroCostos  $unidad_CentroCostos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Unidad_CentroCostos $unidad_CentroCostos)
+    {
+        //
+    }
+}
