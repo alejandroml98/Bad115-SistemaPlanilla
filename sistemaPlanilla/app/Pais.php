0ef24eab89bb86f28335497ba25d7fa711b5d717
PDePaz <pvelasquez@applaudostudios.com>
2021-06-04 18:40:43 -0600

    :sparkles::card_file_box::hammer: 16 CRUD Pais

--- sistemaPlanilla/app/Pais.php
+++ sistemaPlanilla/app/Pais.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pais extends Model
 {
     protected $primaryKey = "idPais";
+
+    public function tipoRegion() {
+        return $this -> belongsTo(TipoRegion::class);
+    }
+    public function region() {
+        return $this -> hasMany(Region::class);
+    }
 }
