447139155e4ab23a23bb4795c8c47e89b64058d6
Lidia González <lidiagp98@gmail.com>
2021-06-08 00:05:22 -0600

    :construction: WIP Empresa agregar direccion en el mismo formulario y agregar reloj

--- sistemaPlanilla/resources/views/empresa/form.blade.php
+++ sistemaPlanilla/resources/views/empresa/form.blade.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="{{ asset('assets/vendor/select2/select2.css') }}" />
 <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-multiselect/bootstrap-multiselect.css') }}" />
 <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-tagsinput/bootstrap-tagsinput.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/vendor/pnotify/pnotify.custom.css') }}" />
 @endpush
 @section('content')
 @if (count($errors) > 0)
@@ -26,18 +27,18 @@
             </header>
             <div class="panel-body p-5">
                 @if ($mode == 'create')
-                <div class="form-row">
+                <div class="form-row pb-3">
                     <div class="form-group col-md-4">
                         <label for="codigoEmpresa">{{ 'Código Empresa' }}</label>
                         <input type="text" name="codigoEmpresa" id="codigoEmpresaCrear" value="{{ old('codigoEmpresaCrear') }}" class="form-control" placeholder="Codigo">
-                        <small id="codigoEmpresa" class="form-text text-muted">El código consta de 2 letras y 5 números.</small>
+                        <small id="codigoEmpresaInstruc" class="form-text text-muted">El código consta de 2 letras y 5 números.</small>
                     </div>
                     <div class="form-group col-md-8">
                         <label for="nombreEmpresa">{{ 'Nombre Empresa' }}</label>
                         <input type="text" name="nombreEmpresa" id="nombreEmpresaCrear" value="{{ old('nombreEmpresaCrear') }}" class="form-control" placeholder="Nombre de la Empresa">
                     </div>
                 </div>
-                <div class="form-row">
+                <div class="form-row pb-3">
                     <div class="form-group col-md-4">
                         <label for="nit">{{ 'NIT' }}</label>
                         <input type="text" name="nit" id="nitCrear" value="{{ old('nitCrear') }}" class="form-control" placeholder="############">
@@ -47,7 +48,7 @@
                         <input type="text" name="nic" id="nicCrear" value="{{ old('nicCrear') }}" class="form-control" placeholder="############">
                     </div>
                 </div>
-                <div class="form-row">
+                <div class="form-row pb-3">
                     <div class="form-group col-md-4">
                         <label for="telefonoEmpresa">{{ 'Teléfono Empresa' }}</label>
                         <input type="text" name="telefonoEmpresa" id="telefonoEmpresaCrear" value="{{ old('telefonoEmpresaCrear') }}" class="form-control" placeholder="####-####">
@@ -57,107 +58,168 @@
                         <input type="text" name="paginaWeb" id="paginaWebCrear" value="{{ old('paginaWebCrear') }}" class="form-control" placeholder="https//:www.empresax.com">
                     </div>
                 </div>
-                <div class="form-group">
+                <div class="form-group pb-3">
                     <label for="correoElectronicoEmpresa">{{ 'Correo Empresa' }}</label>
                     <input type="email" name="correoElectronicoEmpresa" id="correoElectronicoEmpresaCrear" value="{{ old('correoElectronicoEmpresaCrear') }}" class="form-control" placeholder="contacto@empresa.com">
                 </div>
-                <div class="form-group">
-                    <label for="idDireccion">{{ 'Dirección Empresa' }}</label>
-                    <select data-plugin-selectTwo name="idDireccion" id="idDireccionCreate" class="form-control">
-                        @foreach ($direcciones as $direccion)
-                        @foreach ($subRegiones as $subRegion)
-                        @if ($subRegion -> idsubregion == $direccion -> idsubregion)
-                        @foreach ($regiones as $region)
-                        @if ($region -> idregion == $subRegion -> idregion)
-                        @foreach ($paises as $pais)
-                        @if ($pais -> idpais == $region -> idpais)
-                        <option value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
-                        @endif
-                        @endforeach
-                        @endif
-                        @endforeach
-                        @endif
-                        @endforeach
-                        @endforeach
-                    </select>
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-8">
+                        <label for="idDireccion">{{ 'Dirección Empresa' }}</label>
+                        <select data-plugin-selectTwo name="idDireccion" id="idDireccionCreate" class="form-control">
+                            @foreach ($direcciones as $direccion)
+                            @foreach ($subRegiones as $subRegion)
+                            @if ($subRegion -> idsubregion == $direccion -> idsubregion)
+                            @foreach ($regiones as $region)
+                            @if ($region -> idregion == $subRegion -> idregion)
+                            @foreach ($paises as $pais)
+                            @if ($pais -> idpais == $region -> idpais)
+                            <option value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
+                            @endif
+                            @endforeach
+                            @endif
+                            @endforeach
+                            @endif
+                            @endforeach
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-4 align-self-center pt-3">
+                        <a href="#modalCrearDireccion" class="btn modal-with-form btn-success">
+                            Agregar Direccion <i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+                    </div>
                 </div>
-                <div class="form-row">
+
+                <div class="form-row pb-3">
                     <div class="form-group col-md-6">
                         <label for="salarioMinimo">{{ 'Salario Mínimo' }}</label>
                         <input type="number" min="0" max="99999" step=".01" name="salarioMinimo" id="salarioMinimoCreate" value="{{ old('salarioMinimoCreate') }}" class="form-control">
+                    </div>
+                    <div class="form-group col-md-4 align-self-center pt-3 modal-with-form">
+                        <div class="form-check">
+                            <label class="form-check-label pl-5" for="periodoPago">
+                                {{ '¿Paga Mensualmente?' }}
+                            </label>
+                            <input class="form-check-input" type="checkbox" name="periodoPago" id="periodoPagoCreate" value="1">
+                        </div>
+                    </div>
+                </div>
+                @else
+                <input hidden type="text" name="codigoEmpresaAnterior" id="codigoEmpresaAnterior" value="{{ isset($empresa -> codigoempresa)? $empresa -> codigoempresa : old('codigoEmpresaAnterior') }}">
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-4">
+                        <label for="codigoEmpresa">{{ 'Código Empresa' }}</label>
+                        <input type="text" name="codigoEmpresa" id="codigoEmpresa" value="{{ isset($empresa -> codigoempresa)? $empresa -> codigoempresa : old('codigoEmpresa') }}" class="form-control" placeholder="Codigo">
+                        <small id="codigoEmpresaInstrc" class="form-text text-muted">El código consta de 2 letras y 5 números.</small>
+                    </div>
+                    <div class="form-group col-md-8">
+                        <label for="nombreEmpresa">{{ 'Nombre Empresa' }}</label>
+                        <input type="text" name="nombreEmpresa" id="nombreEmpresaCrear" value="{{ isset($empresa -> nombreempresa)? $empresa -> nombreempresa : old('nombreEmpresa') }}" class="form-control" placeholder="Nombre de la Empresa">
+                    </div>
+                </div>
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-4">
+                        <label for="nit">{{ 'NIT' }}</label>
+                        <input type="text" name="nit" id="nit" value="{{ isset($empresa -> nit)? $empresa -> nit : old('nit') }}" class="form-control" placeholder="############">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="nic">{{ 'NIC' }}</label>
+                        <input type="text" name="nic" id="nic" value="{{ isset($empresa -> nic)? $empresa -> nic : old('nic') }}" class="form-control" placeholder="############">
+                    </div>
+                </div>
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-4">
+                        <label for="telefonoEmpresa">{{ 'Teléfono Empresa' }}</label>
+                        <input type="text" name="telefonoEmpresa" id="telefonoEmpresa" value="{{ isset($empresa -> telefonoempresa)? $empresa -> telefonoempresa : old('telefonoEmpresa') }}" class="form-control" placeholder="####-####">
+                    </div>
+                    <div class="form-group col-md-8">
+                        <label for="paginaWeb">{{ 'Página Web Empresa' }}</label>
+                        <input type="text" name="paginaWeb" id="paginaWeb" value="{{ isset($empresa -> paginaweb)? $empresa -> paginaweb : old('paginaWeb') }}" class="form-control" placeholder="https//:www.empresax.com">
+                    </div>
+                </div>
+                <div class="form-group pb-3">
+                    <label for="correoElectronicoEmpresa">{{ 'Correo Empresa' }}</label>
+                    <input type="text" name="correoElectronicoEmpresa" id="correoElectronicoEmpresa" value="{{ isset($empresa -> correoelectronicoempresa)? $empresa -> correoelectronicoempresa : old('correoElectronicoEmpresa') }}" class="form-control" placeholder="contacto@empresa.com">
+                </div>
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-8">
+                        <label for="idDireccion">{{ 'Dirección Empresa' }}</label>
+                        <select data-plugin-selectTwo name="idDireccion" id="idDireccionCreate" class="form-control">
+                            @foreach ($direcciones as $direccion)
+                            @foreach ($subRegiones as $subRegion)
+                            @if ($subRegion -> idsubregion == $direccion -> idsubregion)
+                            @foreach ($regiones as $region)
+                            @if ($region -> idregion == $subRegion -> idregion)
+                            @foreach ($paises as $pais)
+                            @if ($pais -> idpais == $region -> idpais)
+                            @if ($empresa -> iddireccion == $direccion -> iddireccion)
+                            <option selected value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
+                            @else
+                            <option value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
+                            @endif
+                            @endif
+                            @endforeach
+                            @endif
+                            @endforeach
+                            @endif
+                            @endforeach
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-4 align-self-center pt-3 ">
+                        <a href="#modalCrearDireccion" class="btn modal-with-form btn-success">
+                            Agregar Direccion <i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="form-row pb-3">
+                    <div class="form-group col-md-6">
+                        <label for="salarioMinimo">{{ 'Salario Mínimo' }}</label>
+                        <input type="number" min="0" max="99999" step=".01" name="salarioMinimo" id="salarioMinimo" value="{{ isset($empresa -> salariominimo)? $empresa -> salariominimo : old('salarioMinimoCreate') }}" class="form-control">
                     </div>
                     <div class="form-group col-md-4 align-items-center">
                         <div class="form-check">
                             <label class="form-check-label pl-5" for="periodoPago">
                                 {{ '¿Paga Mensualmente?' }}
                             </label>
-                            <input class="form-check-input" type="checkbox" type="checkbox" name="periodoPago" id="periodoPagoCreate" value="1">
+                            @if ($empresa -> periodopago)
+                            <input class="form-check-input" type="checkbox" checked name="periodoPago" id="periodoPago" value="1">
+                            @else
+                            <input class="form-check-input" type="checkbox" name="periodoPago" id="periodoPago" value="1">
+                            @endif
                         </div>
                     </div>
                 </div>
-
-
-                @else
-                <input hidden type="text" name="codigoEmpresaAnterior" id="codigoEmpresaAnterior" value="{{ isset($empresa -> codigoempresa)? $empresa -> codigoempresa : old('codigoEmpresaAnterior') }}">
-                <label for="codigoEmpresa">{{ 'Código Empresa' }}</label>
-                <input type="text" name="codigoEmpresa" id="codigoEmpresa" value="{{ isset($empresa -> codigoempresa)? $empresa -> codigoempresa : old('codigoEmpresa') }}">
-                <label for="nombreEmpresa">{{ 'Nombre Empresa' }}</label>
-                <input type="text" name="nombreEmpresa" id="nombreEmpresaCrear" value="{{ isset($empresa -> nombreempresa)? $empresa -> nombreempresa : old('nombreEmpresa') }}">
-                <label for="nit">{{ 'NIT' }}</label>
-                <input type="text" name="nit" id="nit" value="{{ isset($empresa -> nit)? $empresa -> nit : old('nit') }}">
-                <label for="nic">{{ 'NIC' }}</label>
-                <input type="text" name="nic" id="nic" value="{{ isset($empresa -> nic)? $empresa -> nic : old('nic') }}">
-                <label for="telefonoEmpresa">{{ 'Teléfono Empresa' }}</label>
-                <input type="text" name="telefonoEmpresa" id="telefonoEmpresa" value="{{ isset($empresa -> telefonoempresa)? $empresa -> telefonoempresa : old('telefonoEmpresa') }}">
-                <label for="paginaWeb">{{ 'Página Web Empresa' }}</label>
-                <input type="text" name="paginaWeb" id="paginaWeb" value="{{ isset($empresa -> paginaweb)? $empresa -> paginaweb : old('paginaWeb') }}">
-                <label for="correoElectronicoEmpresa">{{ 'Correo Empresa' }}</label>
-                <input type="text" name="correoElectronicoEmpresa" id="correoElectronicoEmpresa" value="{{ isset($empresa -> correoelectronicoempresa)? $empresa -> correoelectronicoempresa : old('correoElectronicoEmpresa') }}">
-                <label for="idDireccion">{{ 'Dirección Empresa' }}</label>
-                <select name="idDireccion" id="idDireccionCreate">
-                    @foreach ($direcciones as $direccion)
-                    @foreach ($subRegiones as $subRegion)
-                    @if ($subRegion -> idsubregion == $direccion -> idsubregion)
-                    @foreach ($regiones as $region)
-                    @if ($region -> idregion == $subRegion -> idregion)
-                    @foreach ($paises as $pais)
-                    @if ($pais -> idpais == $region -> idpais)
-                    @if ($empresa -> iddireccion == $direccion -> iddireccion)
-                    <option selected value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
-                    @else
-                    <option value="{{ $direccion -> iddireccion }}">{{ $pais -> nombrepais }}, {{ $region -> nombreregion }}, {{ $subRegion -> nombresubregion }}, {{ $direccion -> detalledireccion }}</option>
-                    @endif
-                    @endif
-                    @endforeach
-                    @endif
-                    @endforeach
-                    @endif
-                    @endforeach
-                    @endforeach
-                </select>
-                <label for="periodoPago">{{ '¿Paga Mensualmente?' }}</label>
-                @if ($empresa -> periodopago)
-                <input type="checkbox" checked name="periodoPago" id="periodoPago" value="1">
-                @else
-                <input type="checkbox" name="periodoPago" id="periodoPago" value="1">
                 @endif
-                <label for="salarioMinimo">{{ 'Salario Mínimo' }}</label>
-                <input type="number" min="0" max="99999" step=".01" name="salarioMinimo" id="salarioMinimo" value="{{ isset($empresa -> salariominimo)? $empresa -> salariominimo : old('salarioMinimoCreate') }}">
-                @endif
-                <input type="submit" value="{{ $mode == 'create' ? 'Agregar' : 'Modificar' }}">
-                <a href="{{ url('/empresa/') }}">REGRESAR</a>
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="{{ $mode == 'create' ? 'Agregar' : 'Modificar' }}">
+                    <a class="btn btn-danger" href="{{ url('/empresa/') }}">Cancelar</a>
+                </div>
             </div>
         </section>
+    </div>
+</div>
+
+<!-- Modal Crear Direccion -->
+<div class="modal-block modal-block-primary mfp-hide" id="modalCrearDireccion" tabindex="-1" role="dialog" aria-labelledby="modalCrearDireccion" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <form id="demo-form" class="form-horizontal" action="{{ url('/direccion') }}" method="post">
+                {{ csrf_field() }}
+                @include('direccion.form', ['mode' => 'create'])
+            </form>
+        </div>
     </div>
 </div>
 @endsection
 @push('vendorjs')
 <!-- Specific Page Vendor -->
 <script src="{{ asset('assets/vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.js') }}"></script>
+<script src="{{ asset('assets/vendor/jquery-placeholder/jquery.placeholder.js') }}"></script>
 <script src="{{ asset('assets/vendor/select2/select2.js') }}"></script>
 <script src="{{ asset('assets/vendor/select2/select2_locale_es.js') }}"></script>
 <script src="{{ asset('assets/vendor/bootstrap-multiselect/bootstrap-multiselect.js') }}"></script>
-<script src="js/profesion.js"></script>
+<script src="{{ asset('js/profesion.js')}}"></script>
+<script src="{{ asset('assets/vendor/pnotify/pnotify.custom.js') }}"></script>
+<script src="{{ asset('assets/javascripts/ui-elements/examples.modals.js') }}"></script>
 @if (Session::has('mensaje'))
 <script type="text/javascript">
     mostrarMensaje('{{ Session::get("mensaje") }}');
--- sistemaPlanilla/resources/views/layouts/app.blade.php
+++ sistemaPlanilla/resources/views/layouts/app.blade.php
@@ -58,15 +58,7 @@
             <!-- start: search & user box -->
             <div class="header-right">
                 @auth
-                <form action="pages-search-results.html" class="search nav-form">
-                    <div class="input-group input-search">
-                        <input type="text" class="form-control" name="q" id="q" placeholder="Search...">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
-                        </span>
-                    </div>
-                </form>
-
+                <div id="MyClockDisplay" class="search nav-form text-right" onload="showTime()"></div>
                 <span class="separator"></span>
 
                 <div id="userbox" class="userbox">
