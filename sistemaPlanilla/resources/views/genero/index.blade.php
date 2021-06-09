8a8a667ebd667fd609aa05cbeade96b8cf831b01
Nicknamempty <fermerinonew@gmail.com>
2021-06-08 18:27:49 -0600

    :lipstick: tiporegion

--- sistemaPlanilla/resources/views/genero/index.blade.php
+++ sistemaPlanilla/resources/views/genero/index.blade.php
@@ -1,33 +1,4 @@
-<!-- @if (Session::has('mensaje')){{
-    Session::get('mensaje')
-}}    
-@endif
-<a href="{{ url('/genero/create') }}">Agregar Genero</a>
-<table class="table table-light table-bordered">
-    <thead class="thead-dark">
-        <tr>
-            <th>ID</th>
-            <th>Nombre Genero</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($generos as $genero)
-            <tr>
-                <td>{{ $genero -> idgenero }}</td>
-                <td>{{ $genero -> nombregenero }}</td>
-                <td>
-                    <a href="{{ url('/genero/'.$genero -> idgenero.'/edit') }}" class="btn btn-warning">Editar</a>
-                    <form  method="post" action="{{ url('/genero/'.$genero -> idgenero) }}">                
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-                        <button type="submit" onclick="return confirm('¿Borrar de verdad el registro?')">Eliminar</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table> -->
+
 <!--Nuevo-->
 @extends('layouts.app')
 @push('vendorcss')
@@ -37,7 +8,7 @@
 @endpush
 @section('content')
 <a class="modal-with-form btn btn-primary mb-3" href="#modalCrearProfesion" id="btnCrear">
-    Agregar Profesión <i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+    Agregar Genero <i class="fa fa-plus-circle" aria-hidden="true"></i></a>
 <section class="panel">
     <header class="panel-heading">
         <h2 class="panel-title">Géneros Registrados</h2>
