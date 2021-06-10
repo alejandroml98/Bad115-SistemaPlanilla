1131368fafb397520aa8971e4b86488e82de6be3
PDePaz <pvelasquez@applaudostudios.com>
2021-06-10 01:35:03 -0600

    :sparkles::card_file_box::hammer: Actualizado CRUD Ventas Empleado

--- sistemaPlanilla/resources/views/ventasempleado/form.blade.php
+++ sistemaPlanilla/resources/views/ventasempleado/form.blade.php
@@ -15,37 +15,29 @@
 <label for="codigoEmpleado">{{ 'Codigo del Empleado' }}</label>
 @if ($mode == 'create')
     <select name="codigoEmpleado" id="codigoEmpleado" required>
-        <option value="">Selecciona un empleado</option>
+        <option value="" selected disabled>Selecciona un empleado</option>
         @foreach ($empleados as $empleado)            
-            <option value="{{ $empleado -> codigoempleado }}">{{ $empleado -> codigoempleado }} - {{ $empleado -> codigoempleado }}</option>                
+            <option value="{{ $empleado -> codigoempleado }}">{{ $empleado -> codigoempleado }} - {{ $empleado -> primernombre }}{{ ' ' }}{{ $empleado -> apellidopaterno }}</option>                
         @endforeach
     </select>
+    <label for="valorVenta">{{ 'Valor Venta' }}</label>
+    <input type="number" name="valorVenta" id="valorVentaCreate" value="{{ old('valorVentaCreate') }}">
+    <label for="fechaVenta">{{ 'fecha Venta' }}</label>
+    <input type="date" name="fechaVenta" id="fechaVentaCreate" value="{{ old('fechaVentaCreate') }}">
 @else
-    <select name="codigoEmpleado" id="codigoEmpleado" required>
-        <option value="">Selecciona un Empleado</option>
-        @foreach ($empleados as $empleado)
-            @if (isset($ventaempleado -> idventasempleado))
-                @if ($ventaempleado -> idventasempleado == $empleado -> codigoempleado)
-                    <option value="{{ $empleado -> codigoempleado }}" selected>{{ $empleado-> codigoempleado }} </option>                    
-                @else
-                    <option value="{{ $empleado -> codigoempleado }}">{{ $empleado -> codigoempleado }}</option>                                
-                @endif            
-            @endif
+    <select name="codigoEmpleado" id="codigoEmpleado" required>        
+        @foreach ($empleados as $empleado)            
+            @if ($ventaEmpleado -> codigoempleado == $empleado -> codigoempleado)
+                <option value="{{ $empleado -> codigoempleado }}" selected>{{ $empleado -> codigoempleado }} - {{ $empleado -> primernombre }}{{ ' ' }}{{ $empleado -> apellidopaterno }}</option>
+            @else
+                <option value="{{ $empleado -> codigoempleado }}">{{ $empleado -> codigoempleado }} - {{ $empleado -> primernombre }}{{ ' ' }}{{ $empleado -> apellidopaterno }}</option>
+            @endif                        
         @endforeach
     </select>
-@endif
-
-@if ($mode == 'create')
     <label for="valorVenta">{{ 'Valor Venta' }}</label>
-    <input type="number" name="valorVenta" id="valorVenta" value="{{ old('valorVenta') }}">
+    <input type="number" name="valorVenta" id="valorVenta" value="{{ isset($ventaEmpleado -> valorventa) ? $ventaEmpleado -> valorventa: old('valorVenta') }}">
     <label for="fechaVenta">{{ 'fecha Venta' }}</label>
-    <input type="number" name="fechaVenta" id="fechaVenta" value="{{ old('fechaVenta') }}">
-
-@else
-    <label for="valorVenta">{{ 'Valor Venta' }}</label>
-    <input type="number" name="valorVenta" id="valorVenta" value="{{ isset($ventaEmpleado -> valorVenta) ? $ventaEmpleado -> valorVenta: old('valorVenta') }}">
-    <label for="fechaVenta">{{ 'fecha Venta' }}</label>
-    <input type="number" name="fechaVenta" id="fechaVenta" value="{{ isset($ventaEmpleado -> fechaventa) ? $ventaEmpleado -> fechaventa : old('fechaVenta') }}">
+    <input type="date" name="fechaVenta" id="fechaVenta" value="{{ isset($ventaEmpleado -> fechaventa) ? $ventaEmpleado -> fechaventa -> format('Y-m-d') : old('fechaVenta') }}">
 @endif
 <input type="submit" value="{{ $mode == 'create' ? 'Agregar' : 'Modificar' }}">
 <a href="{{ url('/ventasempleado/') }}">REGRESAR</a>
